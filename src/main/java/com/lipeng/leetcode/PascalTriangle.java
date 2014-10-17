1662eccbc8683bc165e581cea6b70fa280b95dc0
leepengg <pengli.hust@qq.com>
2014-10-17 16:13:00 +0800

    leetcode: PascalTriangle

---
+++ src/main/java/com/lipeng/leetcode/PascalTriangle.java
@@ -0,0 +1,77 @@
+package com.lipeng.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> ret = new ArrayList<List<Integer>>();
+
+		if (numRows <= 0) {
+			return ret;
+		}
+
+		List<Integer> first = new ArrayList<Integer>();
+		first.add(1);
+		ret.add(first);
+
+		if (numRows == 1) {
+			return ret;
+		}
+
+		List<Integer> two = new ArrayList<Integer>();
+		two.add(1);
+		two.add(1);
+		ret.add(two);
+
+		if (numRows == 2) {
+			return ret;
+		}
+
+		int i = 2;
+		List<Integer> tmp = null;
+		List<Integer> pre = null;
+		while (i < numRows) {
+			tmp = new ArrayList<Integer>();
+			pre = ret.get(i - 1);
+
+			tmp.add(1);
+
+			int len = pre.size();
+			for (int j = 0; j < len - 1; j++) {
+				tmp.add(pre.get(j) + pre.get(j + 1));
+			}
+			tmp.add(1);
+
+			ret.add(tmp);
+
+			i++;
+
+		}
+
+		return ret;
+	}
+
+	public static void printRet(List<List<Integer>> ret) {
+		System.out.println(ret.size());
+		for (List<Integer> list : ret) {
+			System.out.println();
+			for (Integer integer : list) {
+				System.out.print(integer);
+				System.out.print(" ");
+			}
+
+		}
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PascalTriangle pt = new PascalTriangle();
+		printRet(pt.generate(5));
+
+	}
+
+}
--- src/main/java/com/lipeng/leetcode/ValidPalindrome.java
+++ src/main/java/com/lipeng/leetcode/ValidPalindrome.java
@@ -1,48 +1,48 @@
 package com.lipeng.leetcode;
 
 public class ValidPalindrome {
-    public boolean isPalindrome(String s) {
-    	if(s == null || s.isEmpty()){
-    		return true;
-    	}
-    	
-    	int i = 0;
-    	int j = s.length()  -1 ;
-    	
-    	while(i < j){
-    		Character first = s.charAt(i);
-        	Character last = s.charAt(j);
-        	
-    		while(!first.isLetterOrDigit(first) && i < j){
-    			i++;
-    			first = s.charAt(i);
-    		}
-    		first = first.toLowerCase(first);
-    		while(!last.isLetterOrDigit(last) && j > i){
-    			j--;
-    			last = s.charAt(j);
-    		}
-    		last = last.toLowerCase(last);
-    		if(first.charValue() != last.charValue()){
-    			return false;
-    		}
-    		
-    		//first = s.charAt(++i);
-    		//last = s.charAt(--j);
-    		++i;
-    		--j;
-    	}
-    	
-    	return true;
-        
-    }
+	public boolean isPalindrome(String s) {
+		if (s == null || s.isEmpty()) {
+			return true;
+		}
+
+		int i = 0;
+		int j = s.length() - 1;
+
+		while (i < j) {
+			Character first = s.charAt(i);
+			Character last = s.charAt(j);
+
+			while (!first.isLetterOrDigit(first) && i < j) {
+				i++;
+				first = s.charAt(i);
+			}
+			first = first.toLowerCase(first);
+			while (!last.isLetterOrDigit(last) && j > i) {
+				j--;
+				last = s.charAt(j);
+			}
+			last = last.toLowerCase(last);
+			if (first.charValue() != last.charValue()) {
+				return false;
+			}
+
+			// first = s.charAt(++i);
+			// last = s.charAt(--j);
+			++i;
+			--j;
+		}
+
+		return true;
+
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ValidPalindrome s = new ValidPalindrome();
-		
+
 		System.out.println(s.isPalindrome("  A man, a plan, a canal: Panama"));
 		System.out.println(s.isPalindrome("race a car"));
 		System.out.println(s.isPalindrome("a."));
