ce9277b5793721eeee84cb432771d442a2dd043a
leepengg <pengli.hust@qq.com>
2014-10-21 20:31:00 +0800

    same tree

---
+++ src/main/java/com/lipeng/leetcode/SameTree.java
@@ -0,0 +1,21 @@
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q== null){
+            return false;
+        }
+        
+        if(p.val != q.val){
+            return false;
+        }
+        
+        if(isSameTree(p.left, q.left) && isSameTree(p.right, q.right)){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
