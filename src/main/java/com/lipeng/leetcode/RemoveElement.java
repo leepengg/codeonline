bfd6db0420f637c8c7c49fb0c3f583eabd023b97
leepengg <pengli.hust@qq.com>
2014-10-23 08:14:04 +0800

    remove element

---
+++ src/main/java/com/lipeng/leetcode/RemoveElement.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if(A == null || A.length == 0){
+            return 0;
+        }
+        
+        int i = 0;
+        int j = A.length;
+        
+        while(i < j){
+            if(A[i] == elem){
+                A[i] = A[j-1];
+                j--;
+            }else{
+                i++;
+            }
+        }
+        
+        return j;
+    }
+}
