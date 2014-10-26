debc5f4617e691b843abc4c7bb7c8d15a20c0f6a
leepengg <pengli.hust@qq.com>
2014-10-27 07:49:00 +0800

    longest common prefix

---
+++ src/main/java/com/lipeng/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null){
+            return null;
+        }
+        if(strs.length == 0){
+            return "";
+        }
+        
+        int i  = 0;
+        StringBuffer sb = new StringBuffer();
+        String tmp = strs[0];
+        boolean flag = true;
+        
+        while(i < tmp.length() && flag == true){
+            char c = tmp.charAt(i);
+            
+            for (String s : strs){
+                if(i< s.length()){
+                    char cc = s.charAt(i);
+                    if(cc == c){
+                         continue;
+                    }else{
+                        flag = false;
+                     }
+                }else{
+                    flag = false;
+                }
+                
+            }
+            
+            if(flag == true){
+                i++;
+            }
+        }
+        
+        return tmp.substring(0, i);
+        
+    }
+}
