d172eb90b6435c91d6b73f87f0db31805bf2d4a9
leepengg <pengli.hust@qq.com>
2014-10-29 19:55:00 +0800

    str str

---
+++ src/main/java/com/lipeng/leetcode/StrStr.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public String strStr(String haystack, String needle) {
+        if(haystack == null || needle == null){
+            return null;
+        }
+        
+        if(haystack.isEmpty()&&needle.isEmpty()){
+            return "";
+        }
+        
+        if(needle.isEmpty()){
+            return haystack;
+        }
+        
+        int lenHaystack = haystack.length();
+        int lenNeedle = needle.length();
+        if(lenHaystack < lenNeedle){
+            return null;
+        }
+        
+        int ret = 0;
+        while(ret < lenHaystack - lenNeedle+1){
+            int i = 0;
+            for(int j=ret; i<lenNeedle; i++,j++){
+                if(haystack.charAt(j) == needle.charAt(i)){
+                    continue;
+                }else{
+                    break;
+                }
+            }
+            if(i == lenNeedle){
+                return haystack.substring(ret);
+            }
+            ret++;
+        }
+        
+        return null;
+        
+    }
+}
